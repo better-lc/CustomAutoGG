8695ac926e1ec3722a5bf5739c75ced3002e581d
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2021-12-02 23:06:08 +0100

    Initial commit

---
+++ src/main/java/me/onils/customautogg/Transformer.java
@@ -0,0 +1,30 @@
+package me.onils.customautogg;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Transformer implements ClassFileTransformer {
+    String ggMessage;
+    Transformer(String ggMsg){
+        this.ggMessage = ggMsg;
+    }
+
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        if (classfileBuffer == null || classfileBuffer.length == 0) {
+            return new byte[0];
+        }
+
+
+        return classfileBuffer;
+    }
+}
